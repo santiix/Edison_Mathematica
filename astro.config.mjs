a0d3a4ebaf44cab29dcff8248193db10ae7bcd6b
Charlie Lopez <santiix@hotmail.com>
2025-05-23 20:52:35 -0400

    Update site with latest files and fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-//export default defineConfig({});
+export default defineConfig({});
 
-//import-site-map
-import sitemap from "@astrojs/sitemap";
-
-export default defineConfig({
-  integrations: [sitemap()],
-  site: 'https://www.life-calculators.com'
-});
 
