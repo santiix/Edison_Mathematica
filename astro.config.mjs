38a346836c3012e3c81323839c601f881418360c
Charlie Lopez <santiix@hotmail.com>
2025-05-23 21:00:38 -0400

    Fix astro.config.mjs config for SEO or sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
-
+export default defineConfig({
+  site: 'https://www.life-calculators.com'
+});
 
