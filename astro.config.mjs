fb94a4e278d572e59f322ce1fc564a0b26cd5f91
Charlie Lopez <santiix@hotmail.com>
2025-05-23 20:37:33 -0400

    Add robots.txt and sitemap configuration for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,3 +3,12 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+
+//import-site-map
+import sitemap from "@astrojs/sitemap";
+
+export default defineConfig({
+  integrations: [sitemap()],
+  site: 'https://www.life-calculators.com'
+});
+
